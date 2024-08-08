2012efc65b0223e2cbc5763c1dcdba8fa2c14f13
Andreanapolicy <daa.and@mail.ru>
2024-08-08 12:36:22 +0300

    Add mutex for organize output

--- multithreading/main.cpp
+++ multithreading/main.cpp
@@ -1,9 +1,14 @@
 #include "ThreadPool.h"
 #include <iostream>
+#include <mutex>
+
+std::mutex g_cout_mutex;
 
 void SayHello()
 {
+	g_cout_mutex.lock();
 	std::cout << "Hello" << std::endl;
+	g_cout_mutex.unlock();
 }
 
 int main()
