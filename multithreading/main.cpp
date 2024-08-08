fb95403fc078c072eb17b2bd5e37e2dbf50eea24
Andreanapolicy <daa.and@mail.ru>
2024-08-08 13:35:49 +0300

    Clear main of main project, add reference of my database

--- multithreading/main.cpp
+++ multithreading/main.cpp
@@ -1,18 +1,8 @@
-#include "ThreadPool.h"
 #include <iostream>
-#include <mutex>
-
-std::mutex g_cout_mutex;
-
-void SayHello()
-{
-	g_cout_mutex.lock();
-	std::cout << "Hello" << std::endl;
-	g_cout_mutex.unlock();
-}
+#include "../database/api/Api.h"
 
 int main()
 {
-	thread::ThreadPool threadPool{ 100, SayHello };
+	const auto database = database::api::GetDatabase("fuck");
 	return 0;
 }
