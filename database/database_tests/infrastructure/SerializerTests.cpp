d8b0865ac4089c174118fc489d5b8f6bd48dc4e3
Andreanapolicy <daa.and@mail.ru>
2024-08-11 14:16:44 +0300

    Added simple tests for Serializer

--- database/database_tests/infrastructure/SerializerTests.cpp
+++ database/database_tests/infrastructure/SerializerTests.cpp
@@ -1,6 +1,109 @@
 #include "../../../../catch2/catch.hpp"
 
-TEST_CASE("simple")
+#include "../../infrastructure/Serializer.h"
+
+TEST_CASE("Valid database")
 {
-	REQUIRE(2 + 2 == 4);
+	GIVEN("file with valid database")
+	{
+		const auto filename = "/example/full_database.txt";
+		WHEN("parse database")
+		{
+			auto db = database::infrastructure::Serializer::GetDatabase(filename);
+			
+			THEN("db will be not empty")
+			{
+				REQUIRE(!db.empty());
+				REQUIRE(db.size() == 2);
+			}
+
+			THEN("where will be 2 tables")
+			{
+				auto table1 = db["firstTable"];
+				auto table2 = db["secondTable"];
+
+				REQUIRE(table1.size() == 3);
+				REQUIRE(table2.size() == 4);
+			}
+		}
+	}
+}
+
+TEST_CASE("Empty database")
+{
+	GIVEN("file with empty database")
+	{
+		const auto filename = "/example/empty_database.txt";
+		WHEN("parse database")
+		{
+			auto db = database::infrastructure::Serializer::GetDatabase(filename);
+
+			THEN("db will be empty")
+			{
+				REQUIRE(db.empty());
+			}
+		}
+	}
+}
+
+TEST_CASE("Database with empty table")
+{
+	GIVEN("file with empty database")
+	{
+		const auto filename = "/example/empty_table_database.txt";
+		WHEN("parse database")
+		{
+			auto db = database::infrastructure::Serializer::GetDatabase(filename);
+
+			THEN("db will be not empty")
+			{
+				REQUIRE(!db.empty());
+				REQUIRE(db.size() == 2);
+			}
+
+			THEN("first table is empty")
+			{
+				auto table = db["empty"];
+
+				REQUIRE(table.empty());
+			}
+
+			THEN("second table has values")
+			{
+				auto table = db["nonempty"];
+
+				REQUIRE(table.size() == 1);
+			}
+		}
+	}
+}
+
+TEST_CASE("Invalid database")
+{
+	GIVEN("file with invalid database")
+	{
+		const auto filename = "/example/invalid_database.txt";
+		WHEN("parse database")
+		{
+			THEN("will be exception")
+			{
+				REQUIRE_THROWS(database::infrastructure::Serializer::GetDatabase(filename));
+			}
+		}
+	}
+}
+
+TEST_CASE("Non exist database")
+{
+	GIVEN("non exist file with database")
+	{
+		const auto filename = "/example/nonexist_database.txt";
+		WHEN("parse database")
+		{
+			THEN("will be exception")
+			{
+				REQUIRE_THROWS(database::infrastructure::Serializer::GetDatabase(filename));
+			}
+		}
+	}
 }
